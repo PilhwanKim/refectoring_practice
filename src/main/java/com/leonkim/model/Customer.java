5a2c2c6ed7b6f5d4725643aad7a6d2550144abb1
leon kim <leon0517@gmail.com>
2017-05-05 08:25:27 +0900

    Chapter 1. Refactoring, a First Example Decomposing and Redistributing the Statement Method Extract Method

--- src/main/java/com/leonkim/model/Customer.java
+++ src/main/java/com/leonkim/model/Customer.java
@@ -28,23 +28,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = rentals.nextElement();
 
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if(each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            thisAmount = amountFor(each);
 
             frequentRenterPoints ++;
 
@@ -58,6 +42,28 @@ public class Customer {
 
         result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
         result += "적립 포인트: " + String.valueOf(frequentRenterPoints) + "\n";
+        return result;
+    }
+
+    private double amountFor(Rental rental) {
+        double result = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if(rental.getDaysRented() > 2) {
+                    result += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(rental.getDaysRented() > 3) {
+                    result += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
         return result;
     }
 
