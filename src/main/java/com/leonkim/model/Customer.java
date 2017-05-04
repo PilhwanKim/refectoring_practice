835a03a760ffeca60e55e9b5330fb7344863df80
leon kim <leon0517@gmail.com>
2017-05-05 08:58:14 +0900

    Chapter 1. Refactoring, a First Example Decomposing and Redistributing the Statement Method add htmlStatement method

--- src/main/java/com/leonkim/model/Customer.java
+++ src/main/java/com/leonkim/model/Customer.java
@@ -35,6 +35,21 @@ public class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "<h1><em>" + getName() + " 고객님의 대여 기록</em></h1><p>\n";
+        while(rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            // 모든 대여 비디오 정보와 대여료를 출력
+            result += each.getMovie().getTitle()+ ": " +
+                    String.valueOf(each.getCharge()) + "<br>\n";
+        }
+        //푸터 행 추가
+        result += "<p>누적 대여료: <em>" + String.valueOf(getTotalCharge()) + "</em><p>\n";
+        result += "적립 포인트: <em>" + String.valueOf(getTotalFrequentRenterPoints()) + "</em><p>\n";
+        return result;
+    }
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration<Rental> rentals = _rentals.elements();
---
+++ src/test/java/com/leonkim/StatementTests.java
@@ -0,0 +1,4 @@
+package com.leonkim;
+
+public class StatementTests {
+}
