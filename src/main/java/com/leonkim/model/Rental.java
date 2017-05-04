c597100a0086ca58cb88934128b5aeea464cf98f
leon kim <leon0517@gmail.com>
2017-05-05 08:43:48 +0900

    Chapter 1. Refactoring, a First Example Decomposing and Redistributing the Statement Method Extract & Move Method - frequentRenterPoints

--- src/main/java/com/leonkim/model/Rental.java
+++ src/main/java/com/leonkim/model/Rental.java
@@ -38,4 +38,14 @@ public class Rental {
         }
         return result;
     }
+
+    int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        // 적립 포인트를 1 포인트 증가
+        frequentRenterPoints ++;
+        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                getDaysRented() > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
