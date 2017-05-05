a1b4d836d73056e47ea73fcbec786b48c86d1df3
leon kim <leon0517@gmail.com>
2017-05-05 09:19:54 +0900

    Chapter 1. Refactoring, a First Example Replacing the Conditional Logic on Price Code with Polymorphism Move Method from Rental to Movie(charge & frequentRenterPoints)

--- src/main/java/com/leonkim/model/Rental.java
+++ src/main/java/com/leonkim/model/Rental.java
@@ -18,34 +18,10 @@ public class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if(getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if(getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     int getFrequentRenterPoints() {
-        int frequentRenterPoints = 0;
-        // 적립 포인트를 1 포인트 증가
-        frequentRenterPoints ++;
-        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1) frequentRenterPoints++;
-        return frequentRenterPoints;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
